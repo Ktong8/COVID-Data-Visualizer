4e58a415e8d29025355227565a6fda06ec0e4a2e
Kevin Tong <kevintong@C02CJ5N6MD6M.tld>
2020-09-20 08:30:40 -0700

    Updates

---
+++ precache-manifest.3c03241fac7f956701e4404aab2b455e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6140b8792ce2f39dc6f5bd66cd6f80b4",
+    "url": "/covid-data-visualizer/index.html"
+  },
+  {
+    "revision": "a0ea82ec32fc00327bc8",
+    "url": "/covid-data-visualizer/static/css/main.a6c8bee0.chunk.css"
+  },
+  {
+    "revision": "30baaa069a3f54f35496",
+    "url": "/covid-data-visualizer/static/js/2.50eac938.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-data-visualizer/static/js/2.50eac938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ea82ec32fc00327bc8",
+    "url": "/covid-data-visualizer/static/js/main.73cd8c27.chunk.js"
+  },
+  {
+    "revision": "5332ed6b24359bc73d37",
+    "url": "/covid-data-visualizer/static/js/runtime-main.1432ab01.js"
+  }
+]);
